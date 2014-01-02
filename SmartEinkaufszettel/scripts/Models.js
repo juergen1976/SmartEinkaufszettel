28b3defabb2a82c04eb23e0ba226b171f5e85125
juergen <juergen.toth@gmx.de>
2014-01-02 05:30:11 +0100

    new

---
+++ SmartEinkaufszettel/scripts/Models.js
@@ -0,0 +1,26 @@
+﻿var Category = kendo.data.Model.define({
+    id: "id",
+    fields: {
+        "name": {
+            type: "string"
+        }
+    }
+});
+
+var Product = kendo.data.Model.define({
+    id: "id",
+    fields: {
+        "name": {
+            type: "string"
+        },
+        "minCount": {
+            type: "number"
+        },
+        "actualCount": {
+            type: "number"
+        },
+        "categoryId": {
+            type: "number"
+        }
+    }
+});
